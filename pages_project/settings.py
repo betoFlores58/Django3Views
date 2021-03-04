a114748aa0ba755fe0a6c542f5b0ea1e03683391
betoFlores58 <albertfr@live.com.mx>
2021-03-03 18:54:18 -0600

    Cambios heroku

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 's)_n4=3548jo1(blhjpl$lti^gb3&qw*ludnf$k%1%=)ztj_y('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
